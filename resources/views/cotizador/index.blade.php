9fb881766af9da67d05ca62bff8fcf1761f0062c
Programador <programadorfuturamatires@gmail.com>
2025-07-30 16:18:08 -0600

    Alineación de stocks en datatables

--- resources/views/cotizador/index.blade.php
+++ resources/views/cotizador/index.blade.php
@@ -696,7 +696,11 @@
                 },
                 fixedColumns: {
                     left: 2 // Congela las primeras 2 columnas
-                }
+                },
+                columnDefs: [{
+                    className: 'text-center',
+                    targets: [12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+                }, ],
             });
 
             // Botón para aplicar filtros
